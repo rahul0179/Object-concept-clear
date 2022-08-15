cac082055b812f371a9b1c2abddbb90f4fe9fb4f
Rahul barman <75018120+rahul0179@users.noreply.github.com>
2022-08-16 01:03:17 +0600

    methodjs

--- method.js
+++ method.js
@@ -1,6 +1,7 @@
 const t = {
     id: 18,
     name: 'rahul barman',
+    money: 10000,
     mejor: 'CSE',
     subject: ['bangla', 'math', 'english', 'physic'],
 
@@ -11,6 +12,14 @@ const t = {
     },
     take: function (id) {
         console.log('student name :', this.name, id);
+    },
+    tret: function (expense) {
+        this.money = this.money - expense;
+        return this.money;
     }
+
+
 }
-t.take(18)
+t.take(18);
+const tret = t.tret(2000)
+console.log(tret)
